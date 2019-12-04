90712bd000d530520fc66962dbeb557b2d9a8a9d
Yash Parekh <parekhyashj@gmail.com>
2019-12-04 16:05:43 -0500

    volley request inside the LoginActivity

--- app/src/main/java/com/example/examination_portal/LoginActivity.java
+++ app/src/main/java/com/example/examination_portal/LoginActivity.java
@@ -5,18 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.RequestFuture;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.StringReader;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 //    RadioButton larbExamee,larbExameOrganizer;
     EditText laetEmail,laetPassword;
     Button labtnLogin;
     TextView latvForgotPassword,latvRegister;
+    ProgressBar loading;
+
+    private String URL_LOGIN = "http://192.168.0.24/android_scripts/connection1.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +52,13 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private void eventsManagement() {
         latvRegister.setOnClickListener(this);
         latvForgotPassword.setOnClickListener(this);
+        labtnLogin.setOnClickListener(this);
     }
 
     private void initialization() {
 //        larbExamee = findViewById(R.id.larbExamee);
 //        larbExameOrganizer = findViewById(R.id.larbExameOrganizer);
+        loading = findViewById(R.id.loading);
         laetEmail = findViewById(R.id.laetEmail);
         laetPassword = findViewById(R.id.laetPassword);
         labtnLogin = findViewById(R.id.labtnLogin);
@@ -61,7 +84,52 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 Toast.makeText(this,"Forgot Password?",Toast.LENGTH_LONG).show();
                 break;
             }
+
+            case R.id.labtnLogin:{
+                performLogin();
+            }
         }
+    }
+
+    private void performLogin() {
+//        make progressbar visible
+        loading.setVisibility(View.VISIBLE);
+
+        final String useremail = this.laetEmail.getText().toString().trim();
+        final String password = this.laetPassword.getText().toString().trim();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, URL_LOGIN, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try{
+                    JSONObject jsonObject = new JSONObject(response);
+                    String success = jsonObject.getString("success");
+                    loading.setVisibility(View.GONE);
+
+                    Log.e("loginActivity", "onResponse: "+success);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("loginActivity", error.getMessage().toString());
+                        loading.setVisibility(View.GONE);
+                    }
+                });
+////        {
+////            @Override
+////            protected Map<String, String> getParams() throws AuthFailureError {
+////                Map<String,String> params = new HashMap<>();
+////                params.put("useremail",useremail);
+////                params.put("password",password);
+////                return params;
+////            }
+////        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 
     private void goToRegisterActivity() {
---
+++ examination_system_android_questions.sql
@@ -0,0 +1,58 @@
+-- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: examination_system_android
+-- ------------------------------------------------------
+-- Server version	8.0.17
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `questions`
+--
+
+DROP TABLE IF EXISTS `questions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `questions` (
+  `question_id` int(11) NOT NULL AUTO_INCREMENT,
+  `question_test_id` int(11) DEFAULT NULL,
+  `question_sentence` varchar(200) DEFAULT NULL,
+  `question_opt_a` varchar(200) DEFAULT NULL,
+  `question_opt_b` varchar(200) DEFAULT NULL,
+  `question_opt_c` varchar(200) DEFAULT NULL,
+  `question_opt_d` varchar(200) DEFAULT NULL,
+  `question_answer` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`question_id`),
+  KEY `fk_question_test_id_idx` (`question_test_id`),
+  CONSTRAINT `fk_question_test_id` FOREIGN KEY (`question_test_id`) REFERENCES `tests` (`test_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `questions`
+--
+
+LOCK TABLES `questions` WRITE;
+/*!40000 ALTER TABLE `questions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `questions` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-12-04 11:11:59
